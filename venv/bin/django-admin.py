e9ed3273dd9eb680919e5f7d7b1ef294e920b0e2
Erwol <ernes.wo@gmail.com>
2016-06-03 19:35:24 +0200

    Actualizado venv a python 2.7 con Django a 1.9

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/erwol/Dropbox/Workspaces/pycharm/sd/filmscrapping/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
