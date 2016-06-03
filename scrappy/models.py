3fe24194709ec5bb30b53384f532fd94ab46d387
Erwol <ernes.wo@gmail.com>
2016-06-03 20:01:53 +0200

    .GitIgnore

--- scrappy/models.py
+++ scrappy/models.py
@@ -1,5 +1,39 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Film(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=100, unique=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Origin(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=50, unique=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Score(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    origin = models.ForeignKey(Origin, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.score, " a la película ", self.film.name, " el ", self.created_at, "."
